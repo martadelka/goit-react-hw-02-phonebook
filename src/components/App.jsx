ee820ec97a81f2d4188ac52ac3ebd1567161c217
martadelka <oamedvedeva777@gmail.com>
2023-11-18 23:48:50 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react'
+
+import { nanoid } from 'nanoid'
+
+export class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+    contacts: [],
+    filter: ''
+    }
+  }
+
+  render() {
+    return (
+      <div>Text</div>
+    )
+  }
+}
